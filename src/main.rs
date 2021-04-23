b5d8f1e49355843c3c53b63e315da73b5b65a0ab
Shahrayar <shahrayarmalik86@gmail.com>
2021-04-23 22:32:46 +0500

    Enum in Rust

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,29 @@
 // mod variables;
 // mod data_types;
 // mod string;
-mod tuples;
+// mod tuples;
 // mod array;
 // mod vectors;
 // mod conditionals;
 // mod loops;
 // mod functions;
-mod structure;
+// mod structure;
+
+
+enum Direction{
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT
+}
+
 
 fn main() {
     // print_formatting::message();
     // variables::understand_variables();
     // data_types::types();
     // string::string();
-    tuples::tuple();
+    // tuples::tuple();
     // array::array();
     // vectors::vector();
     // conditionals::condition();
@@ -24,4 +33,14 @@ fn main() {
     // functions::run();
     // structure::run();
 
+    let player_direction : Direction = Direction::LEFT;
+
+    match player_direction{
+        Direction::UP => println!("Moving up"),
+        Direction::DOWN => println!("Moving down"),
+        Direction::RIGHT => println!("Moving right"),
+        Direction::LEFT => println!("Moving left")
+    }
+
 }
+
---
+++ src/structure.rs
@@ -0,0 +1,18 @@
+// // Structure ---> 'Struct' keyword is used to create custom datatype
+
+// struct Person
+// {
+//     first_name: String,
+//     last_name: String
+// }
+
+// pub fn run()
+// {
+//     let mut p1 = Person{
+//         first_name : "Muhammad",
+//         last_name : "Shahrayar",
+//     };
+
+//     println!("Person is: {}", {p1.first_name} , {p1.last_name});
+// }
+
